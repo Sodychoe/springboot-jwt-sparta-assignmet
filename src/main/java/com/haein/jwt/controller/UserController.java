3481d9bc149f7cbb633d55653731fdfb0d094470
sodychoe <sodychoe@icloud.com>
2025-06-10 01:01:46 +0900

    feat : signup 컨트롤러 메서드 추가

---
+++ src/main/java/com/haein/jwt/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.haein.jwt.controller;
+
+import com.haein.jwt.controller.dto.request.SignupRequest;
+import com.haein.jwt.controller.dto.response.SignupResponse;
+import com.haein.jwt.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping("/signup")
+  public SignupResponse signup(@RequestBody SignupRequest request) {
+    return SignupResponse.from(userService.signup(request.toApplicationDto(request)));
+  }
+}
