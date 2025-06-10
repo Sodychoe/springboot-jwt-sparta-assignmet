8c021a1f0573b50923c7fcace9fb77a0af224b09
sodychoe <sodychoe@icloud.com>
2025-06-10 15:35:15 +0900

    feat : 회원가입 비즈니스 로직 구현

---
+++ src/main/java/com/haein/jwt/repository/UserRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.haein.jwt.repository;
+
+import com.haein.jwt.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName = "singleton")
+public class UserRepositoryImpl implements UserRepository {
+
+  private final List<User> entities = new ArrayList<>();
+  private final AtomicLong idGenerator = new AtomicLong(1L);
+
+  @Override
+  public User save(User user) {
+    long id = idGenerator.getAndIncrement();
+    user.setId(id);
+    entities.add(user);
+
+    return user;
+  }
+
+  @Override
+  public boolean existsByUsername(String username) {
+    return entities.stream()
+        .anyMatch(user -> user.getUsername().equals(username));
+  }
+}
