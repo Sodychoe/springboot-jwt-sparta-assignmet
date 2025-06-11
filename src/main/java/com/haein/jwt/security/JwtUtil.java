3e93708342a7f5fdb86bc52a33802c8d94f907dd
sodychoe <sodychoe@icloud.com>
2025-06-11 16:35:03 +0900

    feat : 에러 처리 시 로그 추가

--- src/main/java/com/haein/jwt/security/JwtUtil.java
+++ src/main/java/com/haein/jwt/security/JwtUtil.java
@@ -11,10 +11,12 @@ import io.jsonwebtoken.security.SignatureException;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtUtil {
 
   private static final String BEARER_PREFIX = "Bearer ";
@@ -54,12 +56,16 @@ public class JwtUtil {
     try {
       Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(removeTokenPrefix(token));
     } catch (SecurityException | MalformedJwtException | SignatureException e) {
+      log.error("토큰 손상, cause = {}", e.toString());
       throw JwtException.from(SecurityErrorCode.INVALID_SIGNATURE);
     } catch (ExpiredJwtException e) {
+      log.error("토큰 유효기간 만료, cause = {}", e.toString());
       throw JwtException.from(SecurityErrorCode.ACCESS_TOKEN_EXPIRED);
     } catch (UnsupportedJwtException e) {
+      log.error("지원하지 않는 JWS 토큰, cause = {}", e.toString());
       throw JwtException.from(SecurityErrorCode.UNSUPPORTED_TOKEN);
     } catch (IllegalArgumentException e) {
+      log.error("토큰의 내용이 비어 있음");
       throw JwtException.from(SecurityErrorCode.EMPTY_TOKEN);
     }
   }
@@ -69,6 +75,6 @@ public class JwtUtil {
       return token.substring(BEARER_PREFIX.length());
     }
 
-    throw new RuntimeException();
+    throw JwtException.from(SecurityErrorCode.INVALID_PREFIX);
   }
 }
