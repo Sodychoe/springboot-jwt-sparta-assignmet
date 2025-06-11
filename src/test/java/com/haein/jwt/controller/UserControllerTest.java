5fbd2b6ac93c96d10111c653b84de2d19833c706
sodychoe <sodychoe@icloud.com>
2025-06-11 20:39:33 +0900

    fix : service 구현에 따른 의존성 변경으로 controller 테스트 모킹 수정

--- src/test/java/com/haein/jwt/controller/UserControllerTest.java
+++ src/test/java/com/haein/jwt/controller/UserControllerTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
@@ -39,7 +39,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
     })
 public class UserControllerTest {
 
-  @SpyBean
+  @MockBean
   UserService userService;
 
   @Autowired
