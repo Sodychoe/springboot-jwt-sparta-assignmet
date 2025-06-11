35e92a5df576eb84aa3c65386ba06ef0859f57d2
sodychoe <sodychoe@icloud.com>
2025-06-12 05:57:11 +0900

    fix : 인증 관련 에러 공통응답으로 처리

--- src/main/java/com/haein/jwt/controller/exception/CommonErrorResponse.java
+++ src/main/java/com/haein/jwt/controller/exception/CommonErrorResponse.java
@@ -5,6 +5,8 @@ import com.haein.jwt.service.exception.ServiceErrorCode;
 
 public record CommonErrorResponse(Error error) {
 
+  public static final String AUTHENTICATION_FAIL_MESSAGE = "인증에 실패하여 해당 서비스에 접근할 수 없습니다. 관리자에게 문의 바랍니다.";
+
   public static CommonErrorResponse of(ServiceErrorCode serviceErrorCode) {
     return new CommonErrorResponse(
         new Error(serviceErrorCode.getCode(), serviceErrorCode.getMessage()));
@@ -12,7 +14,7 @@ public record CommonErrorResponse(Error error) {
 
   public static CommonErrorResponse of(SecurityErrorCode securityErrorCode) {
     return new CommonErrorResponse(
-        new Error(securityErrorCode.getCode(), securityErrorCode.getMessage()));
+        new Error(securityErrorCode.getCode(), AUTHENTICATION_FAIL_MESSAGE));
   }
 
   private record Error(
