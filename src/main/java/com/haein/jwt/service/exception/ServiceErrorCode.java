dbb55a5b9b5686adc3b8c071de920e6778391906
sodychoe <sodychoe@icloud.com>
2025-06-12 19:01:35 +0900

    feat : 관리자 API 에 접근하지 못하는 에러를 보안 에러코드에서 서비스 에러코드로 이동

--- src/main/java/com/haein/jwt/service/exception/ServiceErrorCode.java
+++ src/main/java/com/haein/jwt/service/exception/ServiceErrorCode.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum ServiceErrorCode {
+public enum ServiceErrorCode{
   USER_ALREADY_EXISTS("USER_ALREADY_EXISTS", "이미 가입된 사용자입니다.", HttpStatus.CONFLICT.value()),
   INVALID_CREDENTIALS("INVALID_CREDENTIALS", "아이디 또는 비밀번호가 올바르지 않습니다.", HttpStatus.UNAUTHORIZED.value()),
   USER_NOT_FOUND("USER_NOT_FOUND", "존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND.value()),
-  USER_ALREADY_ADMIN("USER_ALREADY_ADMIN", "요청한 사용자는 이미 관리자입니다.",  HttpStatus.BAD_REQUEST.value());
+  USER_ALREADY_ADMIN("USER_ALREADY_ADMIN", "요청한 사용자는 이미 관리자입니다.",  HttpStatus.BAD_REQUEST.value()),
+  NO_AUTHORIZATION("NO_AUTHORIZATION", "주어진 권한으로 접근할 수 없는 서비스입니다.", HttpStatus.FORBIDDEN.value());
 
   private final String code;
   private final String message;
